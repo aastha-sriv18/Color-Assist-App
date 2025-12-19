ebec832b2b89e2de7916eb1c7cc15a23f3e9308a
Fragoner <sengar.shubham@gmail.com>
2025-12-19 20:54:50 +0530

    "Started with the home function"

--- app/src/main/java/com/aastha/colorassistapp/ui/charts/ChartsFragment.java
+++ app/src/main/java/com/aastha/colorassistapp/ui/charts/ChartsFragment.java
@@ -9,7 +9,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
 import com.aastha.colorassistapp.databinding.FragmentChartsBinding;
 
 public class ChartsFragment extends Fragment {
